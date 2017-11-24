5f23f9bf2f929e4cf65b59bf558be97e7d705cd1
Laura Unaeze <l.unaeze5@gmail.com>
2017-11-24 10:39:20 -0700

    Added a comment box. Got AJAX calls to interact with front end.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -8,3 +8,37 @@ $.getJSON("/articles", function(data) {
 		$(".article").append("<p data-id='" + data[i]._id + "'>" + data[i].headline + "<br />" + data[i].summary + "<br />" + data[i].url + "</p>");
 	}
 });
+
+
+// When the user clicks any p tag
+$(document).on("click","p", function() {
+	// make sure the comment box starts off empty
+	$(".comment").empty();
+
+	var articleId = $(this).attr("data-id");
+
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + articleId
+  })
+    // With that done, add the note information to the page
+    .done(function(data) {
+      console.log(data);
+      // The title of the article
+      $(".comment").append("<h2>" + data.headline + "</h2>");
+      // An input to enter a new title
+      $(".comment").append("<input id='titleinput' name='title' >");
+      // A textarea to add a new note body
+      $(".comment").append("<textarea id='bodyinput' name='body'></textarea>");
+      // A button to submit a new note, with the id of the article saved to it
+      $(".comment").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+      // If there's a note in the article
+      if (data.note) {
+        // Place the title of the note in the title input
+        $("#titleinput").val(data.comment.title);
+        // Place the body of the note in the body textarea
+        $("#bodyinput").val(data.comment.body);
+      }
+    });
+});
